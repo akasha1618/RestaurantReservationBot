a881540e36e6c8635362257f43dcc2f39b1e751d
akasha1618 <37626851+akasha1618@users.noreply.github.com>
2019-08-23 11:59:29 +0300

    Add files via upload

---
+++ waiter/actions.py
@@ -0,0 +1,57 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message("Hello World!")
+#
+#         return []
+
+
+
+from rasa_core_sdk import Action
+from rasa_core_sdk.events import SlotSet
+from bot import RestaurantAPI
+
+
+class ActionSearchRestaurants(Action):
+    def name(self):
+        return 'action_search_restaurants'
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message("looking for restaurants")
+        restaurant_api = RestaurantAPI()
+        restaurants = restaurant_api.search(tracker.get_slot("cuisine"))
+        return [SlotSet("matches", restaurants)]
+
+
+class ActionSuggest(Action):
+    def name(self):
+        return 'action_suggest'
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message("here's what I found:")
+        dispatcher.utter_message(tracker.get_slot("matches"))
+        dispatcher.utter_message("is it ok for you? "
+                                 "hint: I'm not going to "
+                                 "find anything else :)")
+        return []
---
+++ waiter/nmr.py
@@ -0,0 +1,26 @@
+    
+""" main function of numero calculation of matrix by python """
+import datetime
+from addDigit.AddDigits import AddDigits
+from telnetlib import Telnet
+import sys
+
+
+# --- main def ---
+def main(argv):
+    calc_date = datetime.datetime.now()
+    add_digs = AddDigits()
+    add_digs.calc_digits(calc_date)
+    print(" calculated date:", calc_date.date())
+    print("dd-mm-yyyy:", calc_date.day, calc_date.month, calc_date.year)
+    print("ADs:")
+    print(add_digs.AD_01.value, add_digs.AD_02.value)
+    print(add_digs.AD_03.value, add_digs.AD_04.value)
+
+
+    pass
+# --- main def ---
+
+# --- Call of Main function ---
+if __name__ == '__main__':
+    main(sys.argv)
